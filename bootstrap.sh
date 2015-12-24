0fe36c9543dbb28fe4eb99c4d013e9fe50a53410
skyrus <skyrus@gmail.com>
2015-12-24 11:10:38 -0800

    fixing some vagrant stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,5 +9,7 @@ cd Downloads
 echo "installing miniconda"
 wget --quiet https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh
 bash Miniconda-latest-Linux-x86_64.sh -b
-export PATH=/home/vagrant/miniconda:$PATH
+
+echo "setting up conda default python"
+echo "export PATH=/home/vagrant/miniconda2/bin:$PATH" >> /home/vagrant/.bashrc
 
